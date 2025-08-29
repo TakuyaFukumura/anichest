1c4e50c14e7016336ffd14ea7ebaa4847084c691
takuya-fukumura <blackchoco1114@gmail.com>
2025-08-29 20:59:34 +0900

    update minSdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.anichest.app"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
